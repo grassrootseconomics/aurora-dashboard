804f09c887b0297e77c827d5a9ceb08f62063651
razvan-mrc <87325253+razvan-mrc@users.noreply.github.com>
2023-07-07 17:30:09 +0300

    fix: wrong type & formatting in service parameter

--- src/components/core/modals/HarvestingModal.tsx
+++ src/components/core/modals/HarvestingModal.tsx
@@ -1,20 +1,28 @@
-import React, { FC, useEffect, useState } from 'react';
-import { Button, Modal, Box, Typography, MenuItem, Select, TextField } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+
+import React, { FC, useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { Department } from '@/util/models/BasicDepartment';
+
+import {
+  Box,
+  Button,
+  MenuItem,
+  Modal,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
+
 import { getDepartments, updateNextHarvest } from '@/services/department';
 import { convertToIsoDate, convertToSimpleDate } from '@/util/format/date';
+import { Department } from '@/util/models/BasicDepartment';
 
 interface HarvestingModalProps {
   open: boolean;
   onClose: () => void;
 }
 
-const HarvestingModal: FC<HarvestingModalProps> = ({
-  open,
-  onClose,
-}) => {
+const HarvestingModal: FC<HarvestingModalProps> = ({ open, onClose }) => {
   const { t } = useTranslation('translation');
   const { handleSubmit, control, setValue } = useForm<any>();
   const [departments, setDepartments] = useState<Department[]>();
@@ -23,29 +31,41 @@ const HarvestingModal: FC<HarvestingModalProps> = ({
     onClose();
   };
 
-  const handleConfirm = (data: {idDepartment: number, nextHarvest: string}) => {
+  const handleConfirm = (data: {
+    idDepartment: number;
+    nextHarvest: string;
+  }) => {
     updateNextHarvest(data.idDepartment, convertToIsoDate(data.nextHarvest));
     handleClose();
   };
 
   useEffect(() => {
     getDepartments().then((deps) => {
-        setDepartments(deps);
-        setValue("idDepartment", deps[0].id)
-        setValue("nextHarvest", convertToSimpleDate(deps[0].nextHarvest))
+      setDepartments(deps);
+      setValue('idDepartment', deps[0].id);
+      setValue('nextHarvest', convertToSimpleDate(deps[0].nextHarvest));
     });
-  }, [])
+  }, []);
 
   const handleIdDepartmentChange = (selectedOption: any) => {
-    console.log(selectedOption)
+    console.log(selectedOption);
     // Perform any necessary logic here based on the selectedOption
 
     // Update the value of nextHarvest field
-    setValue('nextHarvest', convertToSimpleDate(departments?.find(d => d.id == selectedOption)?.nextHarvest ?? ""));
+    setValue(
+      'nextHarvest',
+      convertToSimpleDate(
+        departments?.find((d) => d.id == selectedOption)?.nextHarvest ?? ''
+      )
+    );
   };
 
   return (
-    <Modal open={open} onClose={handleClose} aria-labelledby="delete-confirmation-modal">
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="delete-confirmation-modal"
+    >
       <Box
         sx={{
           position: 'absolute',
@@ -60,46 +80,48 @@ const HarvestingModal: FC<HarvestingModalProps> = ({
         className="modal__content"
       >
         <Typography variant="h6" id="delete-confirmation-modal" gutterBottom>
-            {t("modals.harvesting.title")}
+          {t('modals.harvesting.title')}
         </Typography>
         <form className="pulp-form" onSubmit={handleSubmit(handleConfirm)}>
-            <Controller
-                name="idDepartment"
-                control={control}
-                render={({ field }) => (
-                    <div className="producer__row">
-                        <Select 
-                            {...field}
-                            value={field.value || ''} // Pass the value from the field object
-                            onChange={(e) => {
-                              field.onChange(e); // Update the field value
-                              handleIdDepartmentChange(e.target.value); // Call the custom handler
-                            }}                            >
-                            {departments?.map(d => {
-                                return <MenuItem key={d.id} value={d.id}>{d.name}</MenuItem>
-                            })
-                            }
-                        </Select>
-                    </div>
-                )}
-            />  
-            <Controller
-                name="nextHarvest"
-                control={control}
-                defaultValue={control._defaultValues.nextHarvest}
-                render={({ field }) => (
-                    <TextField
-                        fullWidth
-                        {...field}
-                    />   
-                )}
-            />  
-            <Button onClick={handleClose} className="modal__button modal__button--close">
-                {t("buttons.close")}
-            </Button>
-            <Button type="submit" className="modal__button modal__button--save">
-                {t("buttons.save")}
-            </Button>
+          <Controller
+            name="idDepartment"
+            control={control}
+            render={({ field }) => (
+              <div className="producer__row">
+                <Select
+                  {...field}
+                  value={field.value || ''} // Pass the value from the field object
+                  onChange={(e) => {
+                    field.onChange(e); // Update the field value
+                    handleIdDepartmentChange(e.target.value); // Call the custom handler
+                  }}
+                >
+                  {departments?.map((d) => {
+                    return (
+                      <MenuItem key={d.id} value={d.id}>
+                        {d.name}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </div>
+            )}
+          />
+          <Controller
+            name="nextHarvest"
+            control={control}
+            defaultValue={control._defaultValues.nextHarvest}
+            render={({ field }) => <TextField fullWidth {...field} />}
+          />
+          <Button
+            onClick={handleClose}
+            className="modal__button modal__button--close"
+          >
+            {t('buttons.close')}
+          </Button>
+          <Button type="submit" className="modal__button modal__button--save">
+            {t('buttons.save')}
+          </Button>
         </form>
       </Box>
     </Modal>
--- src/services/department.ts
+++ src/services/department.ts
@@ -1,20 +1,21 @@
-import { api, authenticatedApi, AxiosResponseData } from "@/plugins/axios";
-import { Department } from "@/util/models/BasicDepartment";
+import { AxiosResponseData, api, authenticatedApi } from '@/plugins/axios';
+import { Department } from '@/util/models/BasicDepartment';
 
 export const getDepartments = async (): Promise<Department[]> => {
-    const response: AxiosResponseData<any> = await api.get(
-      `/v1/department`
-    );
-    return response.data.data.departments;
+  const response: AxiosResponseData<any> = await api.get(`/v1/department`);
+  return response.data.data.departments;
 };
 
-export const updateNextHarvest = async (id: number, nextHarvest: string): Promise<any> => {
+export const updateNextHarvest = async (
+  id: number,
+  nextHarvest: Date
+): Promise<any> => {
   const response: AxiosResponseData<any> = await authenticatedApi.patch(
-    `/v1/department/${id}`, 
+    `/v1/department/${id}`,
     {
       department: {
-        nextHarvest: nextHarvest
-      }
+        nextHarvest: nextHarvest,
+      },
     }
   );
   return response;
